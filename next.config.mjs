6f1fb2ba022a88c01ea6fb20ce330545be746d71
Karo8870 <cazacuchristian@gmail.com>
2024-07-25 14:17:58 +0300

    Switched from Tauri to Electron

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	reactStrictMode: false,
+	output: 'export',
 	images: {
 		unoptimized: true
-	},
-	distDir: 'out'
+	}
 };
 
 export default nextConfig;
